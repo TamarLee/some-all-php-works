1902df76076c5affc12611c8f715fccd4dd6b71e
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2017-08-14 12:38:47 +0400

    ავტორიზაცია

--- index.php
+++ index.php
@@ -1,24 +1,71 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>მთავარი გვერდი</title>
+	<title>ავტორიზაცია</title>
 	<link rel="stylesheet" type="text/css" href="mystyle.css">
 </head>
 <body>
-<div class="main">
-	<div class="notmain">
-		სახელი: <br>
-		გვარი: <br>
-		პაროლი: <br><br>
-	</div>
-	<div class="notmain">
-		<form action="submit.php" method="post">
-			<input type="text" name="name">
-			<input type="text" name="sname">
-			<input type="password" name="password">
-			<input type="submit" value="გაგზავნა" >
-		</form>
-	</div>
-</div>
+
+<?php
+
+$name = $password = "";
+$nameErr = $passwordErr = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	if(empty($_POST["name"])) {
+		$nameErr = "Name is required";
+	} else {
+		$name = test_input ($_POST["name"]);
+	}
+	if(empty($_POST["password"])) {
+		$passwordErr = "Password is required";
+	} else {
+		$password = test_input ($_POST["password"]);
+	}
+}
+
+function test_input ($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+
+	return $data;
+}
+?>
+
+
+<table>
+	<tr>
+		<th>ავტორიზაცია</th>
+	</tr>
+	<tr>
+		<td>
+			<div class="a1">
+				სახელი:<br>
+				პაროლი:<br>
+				<br>
+				<br>
+			</div>
+			<div class="a2">
+				<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+					<input type="text" name="name">
+					<br>
+					<input type="password" name="password">
+					<br>
+					<br>
+					<input type="submit" value="გაგზავნა">
+				</form>
+			</div>
+		</td>
+	</tr>
+</table>
+
+
 </body>
 </html>
+
+<?php
+echo "თქვენი მონაცემები:<br>";
+echo "$name<br>";
+echo "$password<br>";
+?>
