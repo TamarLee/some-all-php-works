8050ccd2976661e9e10b4cf5fa904c2d940b2e2a
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2017-08-14 18:58:04 +0400

    for ციკლი select-ში

--- index.php
+++ index.php
@@ -1,71 +1,50 @@
+<?php
+$year = "";
+$month = "";
+$day = "";
+$age = 0;
+if(!empty($_GET)){
+$year = $_GET["year"];
+$month = $_GET["month"];
+$day = $_GET["day"];
+
+$age = 2017 - $year;
+if($age < 18){
+echo "თქვენ ხართ არასრულწლოვანი!<br><hr><br>";
+} else {
+echo "თქვენ სრულწლოვანი ხართ! <br><hr>";
+}
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>ავტორიზაცია</title>
-	<link rel="stylesheet" type="text/css" href="mystyle.css">
+<title>დაბადების დღე</title>
 </head>
 <body>
-
-<?php
-
-$name = $password = "";
-$nameErr = $passwordErr = "";
-
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-	if(empty($_POST["name"])) {
-		$nameErr = "Name is required";
-	} else {
-		$name = test_input ($_POST["name"]);
-	}
-	if(empty($_POST["password"])) {
-		$passwordErr = "Password is required";
-	} else {
-		$password = test_input ($_POST["password"]);
-	}
-}
-
-function test_input ($data) {
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-
-	return $data;
-}
-?>
-
-
-<table>
-	<tr>
-		<th>ავტორიზაცია</th>
-	</tr>
-	<tr>
-		<td>
-			<div class="a1">
-				სახელი:<br>
-				პაროლი:<br>
-				<br>
-				<br>
-			</div>
-			<div class="a2">
-				<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-					<input type="text" name="name">
-					<br>
-					<input type="password" name="password">
-					<br>
-					<br>
-					<input type="submit" value="გაგზავნა">
-				</form>
-			</div>
-		</td>
-	</tr>
-</table>
-
+მონიშნეთ დაბადების თარიღი:
+<br><br>
+<form action="/" method="get">
+	<select name="year">
+		<?php for($i = 2017; $i >= 1900; $i--){ ?>
+		<option value="<?=$i?>" <?php if($year==$i){echo "selected";}?>> <?php echo "$i"; ?> </option>
+		<?php }?>
+	</select>
+	<select name="month">
+		<?php for($i=1; $i<=12; $i++){ ?>
+		<option value="<?=$i?>" <?php if($month==$i){echo "selected";}?>><?=$i?></option>
+		<?php } ?>
+	</select>
+	<select name="day">
+		<?php for($i=1; $i<=31; $i++){ ?>
+		<option value="<?=$i?>" <?php if($day==$i){echo "selected";}?>><?=$i?></option>
+		<?php } ?>
+	</select>
+	<input type="submit" value="გაგზავნა">
+</form>
 
 </body>
 </html>
-
-<?php
-echo "თქვენი მონაცემები:<br>";
-echo "$name<br>";
-echo "$password<br>";
-?>
