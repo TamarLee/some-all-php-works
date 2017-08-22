36c1e359ca85b9e23dc196f402b99f6aa654c94a
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2017-08-22 23:46:47 +0400

    bootstrap template

--- index.php
+++ index.php
@@ -1,50 +1,108 @@
-<?php
-$year = "";
-$month = "";
-$day = "";
-$age = 0;
-if(!empty($_GET)){
-$year = $_GET["year"];
-$month = $_GET["month"];
-$day = $_GET["day"];
-
-$age = 2017 - $year;
-if($age < 18){
-echo "თქვენ ხართ არასრულწლოვანი!<br><hr><br>";
-} else {
-echo "თქვენ სრულწლოვანი ხართ! <br><hr>";
-}
-
-}
-
-?>
-
 <!DOCTYPE html>
-<html>
-<head>
-<title>დაბადების დღე</title>
-</head>
-<body>
-მონიშნეთ დაბადების თარიღი:
-<br><br>
-<form action="/" method="get">
-	<select name="year">
-		<?php for($i = 2017; $i >= 1900; $i--){ ?>
-		<option value="<?=$i?>" <?php if($year==$i){echo "selected";}?>> <?php echo "$i"; ?> </option>
-		<?php }?>
-	</select>
-	<select name="month">
-		<?php for($i=1; $i<=12; $i++){ ?>
-		<option value="<?=$i?>" <?php if($month==$i){echo "selected";}?>><?=$i?></option>
-		<?php } ?>
-	</select>
-	<select name="day">
-		<?php for($i=1; $i<=31; $i++){ ?>
-		<option value="<?=$i?>" <?php if($day==$i){echo "selected";}?>><?=$i?></option>
-		<?php } ?>
-	</select>
-	<input type="submit" value="გაგზავნა">
-</form>
+<html lang="en">
 
-</body>
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>წიგნების გამოწერა</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+  <?php include 'database.php';?>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">წიგნების გამოწერა</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <?php foreach($menu as $value){?>
+          <li class="nav-item active">
+          <a class="nav-link" href="#"> <?=$value["name"];?> <span class="sr-only">(current)</span></a>
+          </li>
+          <?php }?>
+        </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-lg-3">
+
+          <h3 class="my-4">კატეგორიები</h3>
+          <div class="list-group">
+            <?php foreach($categories as $value){?>
+            <a href="#" class="list-group-item"><?=$value?></a>
+            <?php }?>
+          </div>
+
+        </div>
+        <!-- /.col-lg-3 -->
+
+        <div class="col-lg-9">
+          
+         <div class="row">
+          <?php foreach($books as $value){ ?>
+            <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="<?=$value["ფოტო"];?>" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#"><?=$value["სახელი"];?></a>
+                  </h4>
+                  <h5><?=$value["ფასი"];?> ლარი</h5>
+                  <p class="card-text"><?=$value["აღწერა"];?></p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                </div>
+              </div>
+            </div>
+            <?php }?>
+            
+
+          </div>
+          <!-- /.row -->
+
+        </div>
+        <!-- /.col-lg-9 -->
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; თამარ მეხრიშვილი 2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  </body>
+
 </html>
